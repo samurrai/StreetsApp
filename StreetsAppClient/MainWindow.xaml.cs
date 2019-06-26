6086084c216dbce34777af3ab1dfc90c85acd681
samurrai <samurrrai.r@gmail.com>
2019-06-26 14:43:56 +0600

    initial

---
+++ StreetsAppClient/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StreetsAppClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            indexesComboBox.Items.Add("010000");
+            indexesComboBox.Items.Add("010003");
+            indexesComboBox.Items.Add("010004");
+            indexesComboBox.Items.Add("010009");
+            indexesComboBox.Items.Add("010010");
+        }
+
+        private void GetStreetsListButtonClick(object sender, RoutedEventArgs e)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345));
+            socket.Send(Encoding.UTF8.GetBytes(indexesComboBox.SelectedItem as string));
+            byte[] buffer = new byte[1024 * 4];
+            socket.Receive(buffer);
+            MessageBox.Show(Encoding.UTF8.GetString(buffer));
+        }
+    }
+}
---
+++ StreetsAppServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace StreetsAppServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345));
+            socket.Listen(100);
+            Thread serverThread = new Thread(ServerThreadProcedure);
+            serverThread.Start(socket);
+        }
+
+        private static void ServerThreadProcedure(object obj)
+        {
+            Socket serverSocket = obj as Socket;
+            Socket clientSocket = serverSocket.Accept();
+            while (true)
+            {
+                byte[] buffer = new byte[1024 * 4];
+                int receiveSize = clientSocket.Receive(buffer);
+                if (receiveSize == 0)
+                {
+                    continue;
+                }
+                XmlDocument document = new XmlDocument();
+                document.Load("data.xml");
+                XmlElement rootElemet = document.DocumentElement;
+                string message = "";
+                foreach (XmlNode node in rootElemet)
+                {
+                    if (node.Name == "street")
+                    {
+                        if (node.ChildNodes[1].Value == Encoding.UTF8.GetString(buffer))
+                        {
+                            message += node.ChildNodes[1].Value + "\n";
+                        }
+                    }
+                }
+                clientSocket.Send(Encoding.UTF8.GetBytes(message));
+            }
+        }
+    }
+}
